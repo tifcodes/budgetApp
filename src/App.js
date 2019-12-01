1eaf9f48a56fd56e8fb7fa035982b213d7d413df
Tiffany Wong <tif.wong@yahoo.com>
2019-11-30 21:12:16 -0500

    change balance formula

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ class App extends Component {
     super();
     this.state = {
       transactionList: [],
-      incomeTotal: [],
-      expenseTotal: [],
       incomeArray: [],
       expenseArray: [],
       userInput: "",
@@ -28,8 +26,6 @@ class App extends Component {
       const newTransaction = [];
       const newIncomeArray = [];
       const newExpenseArray = [];
-      const newIncomeTotal = [];
-      const newExpenseTotal = [];
 
       for (let i in transaction) {
         const individualTransactions = {
@@ -40,14 +36,10 @@ class App extends Component {
 
         if (individualTransactions.transactionObject.type === "income") {
           newIncomeArray.push(individualTransactions.transactionObject.amount)
-          const newIncome = newIncomeArray.reduce((a, b) => parseInt(a) + parseInt(b), 0)
-          newIncomeTotal.push(newIncome)
         }
 
         if (individualTransactions.transactionObject.type === "expense") {
           newExpenseArray.push(individualTransactions.transactionObject.amount)
-          const newExpense = newExpenseArray.reduce((a, b) => parseInt(a) + parseInt(b), 0)
-          newExpenseTotal.push(newExpense)
         }
       }
 
@@ -55,8 +47,6 @@ class App extends Component {
         transactionList: newTransaction,
         incomeArray: newIncomeArray,
         expenseArray: newExpenseArray,
-        incomeTotal: newIncomeTotal,
-        expenseTotal: newExpenseTotal
       })
     })
   }
@@ -88,12 +78,11 @@ class App extends Component {
       type: this.state.type
     }
 
-    if (transactionToBeAdded.userInput !== "" && transactionToBeAdded.amount !== "" && transactionToBeAdded.type !== "") {
+    if (transactionToBeAdded !== "") {
       dbRef.push(transactionToBeAdded)
       this.setState({
         userInput: "",
         amount: "",
-        type: ""
       })
     }
   }
@@ -102,22 +91,9 @@ class App extends Component {
     dbRef.child(event.target.id).remove();
   }
 
-  /* <h4> Total : {this.state.incomeArray.reduce((a, b) => parseInt(a) + parseInt(b), 0)} </h4> */
-
-  handleAdd = (array) => {
-    const totalIncome = array.reduce((a, b) => parseInt(a) + parseInt(b), 0)
-
-    console.log(totalIncome)
-
-    // this.setState({
-    //   total: totalIncome,
-    // })
-  }
-
-
   render() {
-    const finalIncomeTotal = this.state.incomeTotal.pop();
-    const finalExpenseTotal = this.state.expenseTotal.pop();
+    const finalIncomeTotal = this.state.incomeArray.reduce((a, b) => parseInt(a) + parseInt(b), 0)
+    const finalExpenseTotal = this.state.expenseArray.reduce((a, b) => parseInt(a) + parseInt(b), 0)
     const finalTotal = finalIncomeTotal - finalExpenseTotal;
     return (
       <div className="wrapper">
@@ -129,9 +105,9 @@ class App extends Component {
         </select>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="transactionType"></label>
-          <input id="transactionType" type="text" value={this.state.userInput} onChange={this.handleUserInputChange} />
+          <input id="transactionType" type="text" placeholder = "description" value={this.state.userInput} onChange={this.handleUserInputChange} />
           <label htmlFor="transactionAmount"></label>
-          <input id="transactionAmount" type="number" value={this.state.amount} onChange={this.handleAmountChange} />
+          <input id="transactionAmount" type="number" placeholder = "amount" value={this.state.amount} onChange={this.handleAmountChange} />
           <button type="submit"> Add Transaction to List</button>
         </form>
         <div className="flexParent">
@@ -144,13 +120,13 @@ class App extends Component {
                   (isType) ?
 
                     <li key={i}> <i className="fa fa-minus-circle" id={transaction.transactionKey} onClick={this.handleDelete}> </i>{transaction.transactionObject.userInput
-                    } {transaction.transactionObject.amount} </li>
+                    } : {transaction.transactionObject.amount} </li>
 
                     : null
                 )
               })}
             </ul>
-            <h4> Total : {finalIncomeTotal} </h4>
+            <h4 className = "colorPop"> Total : {finalIncomeTotal} </h4>
           </div>
           <div>
             <h2> Expenses </h2>
@@ -161,15 +137,15 @@ class App extends Component {
                   (isType) ?
 
                     <li key={i}> <i className="fa fa-minus-circle" id={transaction.transactionKey} onClick={this.handleDelete}></i>  {transaction.transactionObject.userInput
-                    } {transaction.transactionObject.amount} </li>
+                    } : {transaction.transactionObject.amount} </li>
 
                     : null
                 )
               })}
             </ul>
-            <h4> Total: {finalExpenseTotal} </h4>
+            <h4 className = "colorPop"> Total: {finalExpenseTotal} </h4>
           </div>
-          <h3> Balance: {finalTotal} </h3>
+          <h3 className ="colorPop"> Balance: {finalTotal} </h3>
         </div>
       </div>
     )
