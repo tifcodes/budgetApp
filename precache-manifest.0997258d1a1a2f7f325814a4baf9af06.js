a0a6300438517a84f9aae0e12f5e45086cae6b69
Tiffany Wong <tif.wong@yahoo.com>
2019-12-01 13:31:51 -0500

    Updates

---
+++ precache-manifest.0997258d1a1a2f7f325814a4baf9af06.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b70043e0e971f69e43a6d9f5a54bc6",
+    "url": "/tiffanyWongProjectFive/index.html"
+  },
+  {
+    "revision": "5b717c9272f5ba65f6fe",
+    "url": "/tiffanyWongProjectFive/static/css/2.db6f0201.chunk.css"
+  },
+  {
+    "revision": "9ec99ad0606c839e80f6",
+    "url": "/tiffanyWongProjectFive/static/css/main.6d19963c.chunk.css"
+  },
+  {
+    "revision": "5b717c9272f5ba65f6fe",
+    "url": "/tiffanyWongProjectFive/static/js/2.fc82733e.chunk.js"
+  },
+  {
+    "revision": "9ec99ad0606c839e80f6",
+    "url": "/tiffanyWongProjectFive/static/js/main.67b98105.chunk.js"
+  },
+  {
+    "revision": "df848b9102fa27eac6a3",
+    "url": "/tiffanyWongProjectFive/static/js/runtime-main.7c9c2a9b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/tiffanyWongProjectFive/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/tiffanyWongProjectFive/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/tiffanyWongProjectFive/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/tiffanyWongProjectFive/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/tiffanyWongProjectFive/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
