77ed61aa5cd498193d5bfab18a1c959ec1463471
Tiffany Wong <tif.wong@yahoo.com>
2019-12-03 07:48:59 -0500

    took off login/logout

--- src/Header.js
+++ src/Header.js
@@ -1,53 +1,66 @@
-import React, {Component} from 'react';
-import firebase from './firebase'
+// import React, {Component} from 'react';
+// import firebase from './firebase'
 
 // const dbRef = firebase.database().ref();
-const provider = new firebase.auth.GoogleAuthProvider();
-const auth = firebase.auth();
+// const provider = new firebase.auth.GoogleAuthProvider();
+// const auth = firebase.auth();
 
-class Header extends Component {
-  constructor() {
-    super();
-    this.state= {
-      user: null
-    }
-  }
+// class Header extends Component {
+//   constructor() {
+//     super();
+//     this.state= {
+//       user: null
+//     }
+//   }
 
-  componentDidMount() {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.setState ({
-          user
-        })
-      }
-    })
-  }
+//   componentDidMount() {
+//     auth.onAuthStateChanged((user) => {
+//       if (user) {
+//         this.setState ({
+//           user
+//         })
+//       }
+//     })
+//   }
 
-  login = () => {
-    auth.signInWithPopup(provider).then((result) => {
-      const user = result.user;
-      this.setState({
-        user
-      })
-    })
-  }
+//   login = () => {
+//     auth.signInWithPopup(provider).then((result) => {
+//       const user = result.user;
+//       this.setState({
+//         user
+//       })
+//     })
+//   }
 
-  logout = () => {
-    auth.signOut().then(() => {
-      this.setState({
-        user: null
-      })
-    })
-  }
+//   logout = () => {
+//     auth.signOut().then(() => {
+//       this.setState({
+//         user: null
+//       })
+//     })
+//   }
 
-  render() {
+// function Header() {
+//   render() {
+//       return (
+//         <header>
+//           <h1> budget app</h1>
+//             {this.state.user ? <button onClick={this.logout}> Log Out </button> : <button onClick={this.login}> Log In </button>}
+//         </header>
+//       )
+//   }
+// }
+
+// export default Header;
+
+import React from 'react';
+
+function Header() {
       return (
         <header>
           <h1> budget app</h1>
-            {this.state.user ? <button onClick={this.logout}> Log Out </button> : <button onClick={this.login}> Log In </button>}
         </header>
       )
-  }
 }
 
 export default Header;
