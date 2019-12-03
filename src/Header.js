890ccf37eace3b9bf0b27f90f170f7d6c8320325
Tiffany Wong <tif.wong@yahoo.com>
2019-12-03 11:13:14 -0500

    deleted unwanted files

--- src/Header.js
+++ src/Header.js
@@ -1,65 +1,11 @@
-import React, {Component} from 'react';
-import firebase from './firebase'
+import React from 'react';
 
-const dbRef = firebase.database().ref();
-const provider = new firebase.auth.GoogleAuthProvider();
-const auth = firebase.auth();
-
-class Header extends Component {
-  constructor() {
-    super();
-    this.state= {
-      user: null
-    }
-  }
-
-  componentDidMount() {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.setState ({
-          user
-        })
-      }
-    })
-  }
-
-  login = () => {
-    auth.signInWithPopup(provider).then((result) => {
-      const user = result.user;
-      this.setState({
-        user
-      })
-    })
-  }
-
-  logout = () => {
-    auth.signOut().then(() => {
-      this.setState({
-        user: null
-      })
-    })
-  }
-
-  render() {
-      return (
-        <header>
-          <h1> budget app</h1>
-            {this.state.user ? <button onClick={this.logout}> Log Out </button> : <button onClick={this.login}> Log In </button>}
-        </header>
-      )
-  }
+function Header() {
+  return (
+    <header>
+      <h1> budget app</h1>
+    </header>
+  )
 }
 
 export default Header;
-
-// import React from 'react';
-
-// function Header() {
-//       return (
-//         <header>
-//           <h1> budget app</h1>
-//         </header>
-//       )
-// }
-
-// export default Header;
