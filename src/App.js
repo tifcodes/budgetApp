67156e50500bf085286f8a34020e24f8cadc2d23
Tiffany Wong <tif.wong@yahoo.com>
2019-12-03 08:03:26 -0500

    added login/logout to the app

--- src/App.js
+++ src/App.js
@@ -1,169 +1,3 @@
-// import React, { Component } from 'react';
-// import "./styles/style.scss"
-
-// import Header from "./Header"
-// import firebase from './firebase'
-// import ShowColumn from './Column'
-// import Footer from './Footer'
-
-// const dbRef = firebase.database().ref();
-// // const provider = new firebase.auth.GoogleAuthProvider();
-// // const auth = firebase.auth();
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       transactionList: [],
-//       incomeArray: [],
-//       expenseArray: [],
-//       userInput: "",
-//       amount: "",
-//       type: "",
-//       selectedValue: "",
-//     }
-//   }
-
-// // listening to firebase
-//   componentDidMount() {
-//     dbRef.on("value", (snapshot) => {
-//       const transaction = snapshot.val();
-
-//       const newTransaction = [];
-//       const newIncomeArray = [];
-//       const newExpenseArray = [];
-
-//       for (let i in transaction) {
-//         const individualTransactions = {
-//           transactionKey: i,
-//           transactionObject: transaction[i],
-//         }
-//         newTransaction.push(individualTransactions)
-
-//         if (individualTransactions.transactionObject.type === "income") {
-//           newIncomeArray.push(individualTransactions.transactionObject.amount)
-//         }
-
-//         if (individualTransactions.transactionObject.type === "expense") {
-//           newExpenseArray.push(individualTransactions.transactionObject.amount)
-//         }
-//       }
-
-//       this.setState({
-//         transactionList: newTransaction,
-//         incomeArray: newIncomeArray,
-//         expenseArray: newExpenseArray,
-//       })
-//     })
-
-//     // auth.onAuthStateChanged((user) => {
-//     //   if (user) {
-//     //     this.setState ({
-//     //       user
-//     //     })
-//     //   }
-//     // })
-//   }
-
-// // changing the income and expense drop down
-//   handleChangeType = (event) => {
-//     this.setState({
-//       type: event.target.value
-//     })
-//   }
-
-// // listening to changes in the description
-//   handleUserInputChange = (event) => {
-//     this.setState({
-//       userInput: event.target.value,
-//     })
-//   }
-
-// // listening to changes in the amount
-//   handleAmountChange = (event) => {
-//     this.setState({
-//       amount: event.target.value,
-//     })
-//   }
-
-// // submit 
-//   handleSubmit = (event) => {
-//     event.preventDefault();
-
-//     const transactionToBeAdded = {
-//       userInput: this.state.userInput,
-//       amount: this.state.amount,
-//       type: this.state.type
-//     }
-
-//     if (transactionToBeAdded.userInput !== "" && transactionToBeAdded.amount !== "" && transactionToBeAdded.type !== "") {
-//       dbRef.push(transactionToBeAdded)
-//       this.setState({
-//         userInput: "",
-//         amount: "",
-//       })
-//     }
-//   }
-
-//   // handleReset = (event) => {
-//   //   this.setState({
-//   //     userInput: "",
-//   //     amount: "",
-//   //     type: ""
-//   //   })
-//   // }
-
-//   // login = () => {
-//   //   auth.signInWithPopup(provider).then((result) => {
-//   //     const user = result.user;
-//   //     this.setState({
-//   //       user
-//   //     })
-//   //   })
-//   // }
-
-//   // logout = () => {
-//   //   auth.signOut().then(() => {
-//   //     this.setState({
-//   //       user: null
-//   //     })
-//   //   })
-//   // }
-
-//   render() {
-//     const { userInput, amount } = this.state
-//     const isEnabled = userInput.length > 0 && amount.length > 0
-//     return (
-//       <div className="wrapper">
-//         <Header />
-//         {/* {this.state.user ? <button onClick={this.logout}> Log Out </button> : <button onClick={this.login}> Log In </button>} */}
-//         {/* dropdown menu */}
-//         <main>
-//           <select onChange={this.handleChangeType} value={this.state.value}>
-//             <option value="" > select </option>
-//             <option value="income"> income </option>
-//             <option value="expense"> expense </option>
-//           </select>
-//         {/* form */}
-//           <form onSubmit={this.handleSubmit}>
-//             <label htmlFor="transactionType" className="visuallyHidden"></label>
-//             <input id="transactionType" type="text" placeholder="description" value={this.state.userInput} onChange={this.handleUserInputChange} />
-//             <label htmlFor="transactionAmount" className="visuallyHidden"></label>
-//             <input id="transactionAmount" type="number" placeholder="amount" min="0" step=".01" value={this.state.amount} onChange={this.handleAmountChange} />
-//             <button disabled={!isEnabled} type="submit"> Add Transaction to List</button>
-//             {/* <button type="button" onClick={this.handleReset} > Reset </button> */}
-//           </form>
-//         {/* column */}
-//           <ShowColumn arrayIncome={this.state.incomeArray} arrayExpense={this.state.expenseArray} listTransaction={this.state.transactionList} delete={this.state.handleDelete} />
-//         </main>
-//         <Footer />
-//       </div>
-//     )
-//   }
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 import "./styles/style.scss"
 
@@ -173,6 +7,8 @@ import ShowColumn from './Column'
 import Footer from './Footer'
 
 const dbRef = firebase.database().ref();
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
 
 class App extends Component {
   constructor() {
@@ -219,6 +55,14 @@ class App extends Component {
         expenseArray: newExpenseArray,
       })
     })
+
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState ({
+          user
+        })
+      }
+    })
   }
 
 // changing the income and expense drop down
@@ -261,12 +105,38 @@ class App extends Component {
     }
   }
 
+  // handleReset = (event) => {
+  //   this.setState({
+  //     userInput: "",
+  //     amount: "",
+  //     type: ""
+  //   })
+  // }
+
+  login = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      const user = result.user;
+      this.setState({
+        user
+      })
+    })
+  }
+
+  logout = () => {
+    auth.signOut().then(() => {
+      this.setState({
+        user: null
+      })
+    })
+  }
+
   render() {
     const { userInput, amount } = this.state
     const isEnabled = userInput.length > 0 && amount.length > 0
     return (
       <div className="wrapper">
         <Header />
+        {this.state.user ? <button onClick={this.logout}> Log Out </button> : <button onClick={this.login}> Log In </button>}
         {/* dropdown menu */}
         <main>
           <select onChange={this.handleChangeType} value={this.state.value}>
@@ -281,6 +151,7 @@ class App extends Component {
             <label htmlFor="transactionAmount" className="visuallyHidden"></label>
             <input id="transactionAmount" type="number" placeholder="amount" min="0" step=".01" value={this.state.amount} onChange={this.handleAmountChange} />
             <button disabled={!isEnabled} type="submit"> Add Transaction to List</button>
+            {/* <button type="button" onClick={this.handleReset} > Reset </button> */}
           </form>
         {/* column */}
           <ShowColumn arrayIncome={this.state.incomeArray} arrayExpense={this.state.expenseArray} listTransaction={this.state.transactionList} delete={this.state.handleDelete} />
@@ -292,3 +163,132 @@ class App extends Component {
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import "./styles/style.scss"
+
+// import Header from "./Header"
+// import firebase from './firebase'
+// import ShowColumn from './Column'
+// import Footer from './Footer'
+
+// const dbRef = firebase.database().ref();
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       transactionList: [],
+//       incomeArray: [],
+//       expenseArray: [],
+//       userInput: "",
+//       amount: "",
+//       type: "",
+//       selectedValue: "",
+//     }
+//   }
+
+// // listening to firebase
+//   componentDidMount() {
+//     dbRef.on("value", (snapshot) => {
+//       const transaction = snapshot.val();
+
+//       const newTransaction = [];
+//       const newIncomeArray = [];
+//       const newExpenseArray = [];
+
+//       for (let i in transaction) {
+//         const individualTransactions = {
+//           transactionKey: i,
+//           transactionObject: transaction[i],
+//         }
+//         newTransaction.push(individualTransactions)
+
+//         if (individualTransactions.transactionObject.type === "income") {
+//           newIncomeArray.push(individualTransactions.transactionObject.amount)
+//         }
+
+//         if (individualTransactions.transactionObject.type === "expense") {
+//           newExpenseArray.push(individualTransactions.transactionObject.amount)
+//         }
+//       }
+
+//       this.setState({
+//         transactionList: newTransaction,
+//         incomeArray: newIncomeArray,
+//         expenseArray: newExpenseArray,
+//       })
+//     })
+//   }
+
+// // changing the income and expense drop down
+//   handleChangeType = (event) => {
+//     this.setState({
+//       type: event.target.value
+//     })
+//   }
+
+// // listening to changes in the description
+//   handleUserInputChange = (event) => {
+//     this.setState({
+//       userInput: event.target.value,
+//     })
+//   }
+
+// // listening to changes in the amount
+//   handleAmountChange = (event) => {
+//     this.setState({
+//       amount: event.target.value,
+//     })
+//   }
+
+// // submit 
+//   handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     const transactionToBeAdded = {
+//       userInput: this.state.userInput,
+//       amount: this.state.amount,
+//       type: this.state.type
+//     }
+
+//     if (transactionToBeAdded.userInput !== "" && transactionToBeAdded.amount !== "" && transactionToBeAdded.type !== "") {
+//       dbRef.push(transactionToBeAdded)
+//       this.setState({
+//         userInput: "",
+//         amount: "",
+//       })
+//     }
+//   }
+
+//   render() {
+//     const { userInput, amount } = this.state
+//     const isEnabled = userInput.length > 0 && amount.length > 0
+//     return (
+//       <div className="wrapper">
+//         <Header />
+//         {/* dropdown menu */}
+//         <main>
+//           <select onChange={this.handleChangeType} value={this.state.value}>
+//             <option value="" > select </option>
+//             <option value="income"> income </option>
+//             <option value="expense"> expense </option>
+//           </select>
+//         {/* form */}
+//           <form onSubmit={this.handleSubmit}>
+//             <label htmlFor="transactionType" className="visuallyHidden"></label>
+//             <input id="transactionType" type="text" placeholder="description" value={this.state.userInput} onChange={this.handleUserInputChange} />
+//             <label htmlFor="transactionAmount" className="visuallyHidden"></label>
+//             <input id="transactionAmount" type="number" placeholder="amount" min="0" step=".01" value={this.state.amount} onChange={this.handleAmountChange} />
+//             <button disabled={!isEnabled} type="submit"> Add Transaction to List</button>
+//           </form>
+//         {/* column */}
+//           <ShowColumn arrayIncome={this.state.incomeArray} arrayExpense={this.state.expenseArray} listTransaction={this.state.transactionList} delete={this.state.handleDelete} />
+//         </main>
+//         <Footer />
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
